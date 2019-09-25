69d38fee0d524dd024209c5cacf45704ff2914f9
Venkat Madala <vmadala@prokarma.com>
2019-09-25 21:37:53 +0530

    hi

---
+++ src/main/java/com/springboot/microservice/forex/ForexServiceApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.microservice.forex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexServiceApplication.class, args);
+	}
+
+}
